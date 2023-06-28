e7161ed88c77d9f269a847dca660c5edf23d919a
Harun <mdharunbd88@gmail.com>
2023-06-28 12:39:54 +0600

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h3>{{name}} - {{ age }}</h3>
+  <button @click="changeName('new name')">change name</button>
+  <button @click="changeAge(15)">change Age</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {},
+  data() {
+    return{
+      name: 'Link',
+      age: 25 as string | number
+    }
+  },
+  methods:{
+    changeName(name:string){
+      this.name = name
+      return name
+    },
+    changeAge(age: string | number){
+      this.age = age
+      return age
+    }
+  }
+});
+</script>
+
+<style></style>
